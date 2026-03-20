91bc0d647094d0ffc072c35e56f8a24f2c2ff06c
Akram <akrambenaoun1993@gmail.com>
2026-03-21 00:14:22 +0100

    UPDATE login

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -77,7 +77,7 @@ export default function SignupPage() {
             type="submit"
             className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition"
           >
-            S'inscrire
+            S&apos;inscrire
           </button>
         </form>
       </div>
