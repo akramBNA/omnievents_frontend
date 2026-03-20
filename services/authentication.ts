5c26923a25a2babf0a814ee56aa04dd422ff75a3
Akram <akrambenaoun1993@gmail.com>
2026-03-21 00:07:37 +0100

    add signup method into auth file

--- services/authentication.ts
+++ services/authentication.ts
@@ -20,3 +20,30 @@ export const login = async (email: string, password: string) => {
 
   return data;
 };
+
+export const logout = () => {
+  sessionStorage.removeItem("token");
+};
+
+export const signup = async ( firstName: string, lastName: string, email: string, password: string ) => {
+  const res = await fetch(`${API_URL}/users/signUp`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_name: firstName,
+      user_lastname: lastName,
+      user_email: email,
+      user_password: password,
+    }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Signup failed");
+  }
+
+  return data;
+};
