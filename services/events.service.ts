fc720caf7a3773e0250ac7b5f2f965e72a39bd6f
Akram <akrambenaoun1993@gmail.com>
2026-03-21 22:48:39 +0100

    CREATE events service

---
+++ services/events.service.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const getToken = () => localStorage.getItem("token");
+
+export const eventsService = {
+  async fetchEvents(params: any) {
+    const query = new URLSearchParams(params).toString();
+    const res = await fetch(`${API_URL}/events?${query}`);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(data.message);
+    return data;
+  },
+
+  async createEvent(eventData: any) {
+    const res = await fetch(`${API_URL}/events`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getToken()}`,
+      },
+      body: JSON.stringify(eventData),
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+    return data.data;
+  },
+
+  async updateEvent(id: number, eventData: any) {
+    const res = await fetch(`${API_URL}/events/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getToken()}`,
+      },
+      body: JSON.stringify(eventData),
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+    return data.data;
+  },
+
+  async deleteEvent(id: number) {
+    const res = await fetch(`${API_URL}/events/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    });
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message);
+    return id;
+  },
+};
