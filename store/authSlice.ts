7f5b9f7c5118b5aa9e748b2e3355d35389709f04
Akram <akrambenaoun1993@gmail.com>
2026-03-21 09:17:24 +0100

    CREATE REDUX STORE

---
+++ store/authSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  role: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.token = action.payload.token;
+      state.role = action.payload.role;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.role = null;
+    },
+  },
+});
+
+export const { setUser, logout } = authSlice.actions;
+export default authSlice.reducer;
