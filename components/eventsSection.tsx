116d9edfe6d804a332d5c535dcb30e7e9ffee407
Akram <akrambenaoun1993@gmail.com>
2026-03-22 01:45:02 +0100

    add a loading spinner at the content's section

--- components/eventsSection.tsx
+++ components/eventsSection.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchEvents } from "../store/eventsSlice";
 import { RootState, AppDispatch } from "../store";
+import { debounce } from "lodash";
 
 import EventsTable from "./eventsTable";
 import EventModal from "./eventModal";
-import { debounce } from "lodash";
 
 export default function EventsSection() {
   const dispatch = useDispatch<AppDispatch>();
-  const { events } = useSelector((state: RootState) => state.events);
+  const { events, loading } = useSelector((state: RootState) => state.events);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [search, setSearch] = useState("");
@@ -20,15 +20,25 @@ export default function EventsSection() {
     dispatch(fetchEvents({ limit: 10, offset: 0, keyword: search }));
   }, [dispatch, search]);
 
-  const debouncedSearch = debounce((val: string) => {
-    setSearch(val);
-  }, 500);
+  const debouncedSearch = useMemo(
+    () =>
+      debounce((val: string) => {
+        setSearch(val);
+      }, 500),
+    [],
+  );
+
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [debouncedSearch]);
 
   return (
     <div>
       <div className="flex justify-between items-center mb-4 flex-wrap gap-2 md:ml-64">
-        {" "}
         <h1 className="text-4xl text-white font-bold">Events</h1>
+
         <div className="flex gap-2 flex-wrap">
           <input
             type="text"
@@ -36,6 +46,7 @@ export default function EventsSection() {
             className="p-2 rounded-lg bg-white/90 border border-white shadow focus:outline-none"
             onChange={(e) => debouncedSearch(e.target.value)}
           />
+
           <button
             onClick={() => setIsModalOpen(true)}
             className="bg-gray-200 text-black px-4 py-2 rounded hover:bg-gray-300 cursor-pointer"
@@ -45,7 +56,13 @@ export default function EventsSection() {
         </div>
       </div>
 
-      <EventsTable events={events} />
+      {loading ? (
+        <div className="flex justify-center mt-20">
+          <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      ) : (
+        <EventsTable events={events} />
+      )}
 
       <EventModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
