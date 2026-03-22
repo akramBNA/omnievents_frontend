7b1b1a07f906fe224992a109d7c0c329091c7ca7
Akram <akrambenaoun1993@gmail.com>
2026-03-22 08:11:29 +0100

    UPDATE useAuth hook

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -4,38 +4,20 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getUserFromToken } from "../services/authentication";
-import { useDispatch } from "react-redux";
-import { setUser } from "@/store/authSlice";
 
 export const useAuth = (allowedRoles: string[]) => {
   const router = useRouter();
-  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const payload = getUserFromToken();
-    const token = localStorage.getItem("token");
+    const user = getUserFromToken();
 
-    if (!payload || !allowedRoles.includes(payload.role)) {
+    if (!user || !allowedRoles.includes(user.role)) {
       router.push("/access-denied");
-      return;
+    } else {
+      setLoading(false);
     }
-    dispatch(
-      setUser({
-        user: {
-          user_id: payload.user_id,
-          user_name: payload.user_name || "",
-          user_lastname: payload.user_lastname || "",
-          user_email: payload.user_email || "",
-          user_role_id: payload.role === "super_admin" ? 1 : payload.role === "admin" ? 2 : 3,
-        },
-        token: token || "",
-        role: payload.role,
-      }),
-    );
-
-    setLoading(false);
-  }, [allowedRoles, router, dispatch]);
+  }, [allowedRoles, router]);
 
   return { loading };
 };
